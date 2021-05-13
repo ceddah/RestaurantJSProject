f8730208b2bf43ed252fe88aef3d0c0ebe1dd486
ceddah <cedanik1@gmail.com>
2021-05-14 00:10:48 +0200

    Test

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,53 @@ module.exports = {
     ],
   },
   mode: 'development',
+  target: 'node',
+  resolve: {
+    fallback: {
+      "fs": false,
+      "tls": false,
+      "net": false,
+      "path": false,
+      "zlib": false,
+      "http": false,
+      "https": false,
+      "stream": false,
+      "crypto": false,
+    } 
+  },
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const path = require('path');
+
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     filename: 'main.js',
+//     path: path.resolve(__dirname, 'dist'),
+//   },
+//   module: {
+//     rules: [
+//       {
+//         test: /\.css$/i,
+//         use: ['style-loader', 'css-loader'],
+//       },
+//     ],
+//   },
+//   mode: 'development',
+// };
