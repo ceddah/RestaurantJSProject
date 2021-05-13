513c29907c9161314ba9bac3ff37b06662f4e4dc
ceddah <cedanik1@gmail.com>
2021-05-14 00:41:02 +0200

    Test1

--- webpack.config.js
+++ webpack.config.js
@@ -15,20 +15,6 @@ module.exports = {
     ],
   },
   mode: 'development',
-  target: 'node',
-  resolve: {
-    fallback: {
-      "fs": false,
-      "tls": false,
-      "net": false,
-      "path": false,
-      "zlib": false,
-      "http": false,
-      "https": false,
-      "stream": false,
-      "crypto": false,
-    } 
-  },
 };
 
 
