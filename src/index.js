5cbc565c896072cdc9095f0c6c977a0dbee7b8a9
ceddah <cedanik1@gmail.com>
2021-05-14 00:40:07 +0200

    Exported Content Element

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import './style.css';
 import homeRender from './modules/home.js';
 
+const content = document.getElementById('content');
+
 const RenderPage = () => {
     console.log('rendering page...')
+    homeRender();
 }
 
 window.addEventListener('load', RenderPage);
+
+export { content as default}
