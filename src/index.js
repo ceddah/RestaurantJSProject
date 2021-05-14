f4dc11fa3e8872980537a02cf898263a0d184f7f
ceddah <cedanik1@gmail.com>
2021-05-14 14:46:56 +0200

    day 2

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import homeRender from './modules/home.js';
 const content = document.getElementById('content');
 
 const RenderPage = () => {
-    console.log('rendering page...')
+    console.log('rendering page...333')
     homeRender();
 }
 
