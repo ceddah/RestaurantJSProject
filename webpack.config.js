6aca05cf7e63794f83b8bb2fb320a2f76a0ee4d8
ceddah <cedanik1@gmail.com>
2021-05-14 15:03:18 +0200

    test 4

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: '/src/index.js',entry,
+  entry: './src/index.js',entry,
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
