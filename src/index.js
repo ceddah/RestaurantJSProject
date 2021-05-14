87a7005dc7993652752efe8a9b678800716cd80f
ceddah <cedanik1@gmail.com>
2021-05-14 21:01:28 +0200

    ContactUs Component implemented

--- src/index.js
+++ src/index.js
@@ -4,18 +4,21 @@ import homePageRender from './modules/home.js';
 import homeTab from './modules/homeTab';
 import menuTab from './modules/menuTab';
 import aboutUsTab from './modules/aboutUsTab';
+import contactTab from './modules/contactTab';
 
 const content = document.getElementById('content');
 
 const RenderPage = () => {
     //Header, Footer, Main are included in homePageRender
-    // homePageRender();
+    homePageRender();
 
     // // //Then in Main we append Tabs
-    // const main = document.getElementById('main');
-    // main.appendChild(homeTab());
-    // main.appendChild(menuTab());
-    // main.appendChild(aboutUsTab());
+    const main = document.getElementById('main');
+    main.appendChild(homeTab());
+    main.appendChild(menuTab());
+    main.appendChild(aboutUsTab());
+    main.appendChild(contactTab());
+    console.log('123')
 }
 
 export { content as default};
